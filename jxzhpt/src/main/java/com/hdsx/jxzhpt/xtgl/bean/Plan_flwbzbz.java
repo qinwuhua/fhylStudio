e9e8fbc02bf618304b60b3fc3b38a10a4e783cbc
admin <admin@LrLuckly>
2015-02-10 09:16:34 +0800

    renhao

---
+++ jxzhpt/src/main/java/com/hdsx/jxzhpt/xtgl/bean/Plan_flwbzbz.java
@@ -0,0 +1,46 @@
+package com.hdsx.jxzhpt.xtgl.bean;
+
+public class Plan_flwbzbz {
+	private String id;
+	private String xmlx;
+	private String gldj;
+	private String tsdq;
+	private String bzzj;
+	private String jsdj;
+	public String getJsdj() {
+		return jsdj;
+	}
+	public void setJsdj(String jsdj) {
+		this.jsdj = jsdj;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getXmlx() {
+		return xmlx;
+	}
+	public void setXmlx(String xmlx) {
+		this.xmlx = xmlx;
+	}
+	public String getGldj() {
+		return gldj;
+	}
+	public void setGldj(String gldj) {
+		this.gldj = gldj;
+	}
+	public String getTsdq() {
+		return tsdq;
+	}
+	public void setTsdq(String tsdq) {
+		this.tsdq = tsdq;
+	}
+	public String getBzzj() {
+		return bzzj;
+	}
+	public void setBzzj(String bzzj) {
+		this.bzzj = bzzj;
+	}
+}
