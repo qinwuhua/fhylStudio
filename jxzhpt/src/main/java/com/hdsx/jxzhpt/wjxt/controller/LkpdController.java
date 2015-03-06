2aa8b4781e44b336b36a56f1107942d5771325e7
Administrator <Administrator@N3Q8TJCVDFIDBA6>
2015-03-06 10:52:10 +0800

    qwh

---
+++ jxzhpt/src/main/java/com/hdsx/jxzhpt/wjxt/controller/LkpdController.java
@@ -0,0 +1,163 @@
+package com.hdsx.jxzhpt.wjxt.controller;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.struts2.ServletActionContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hdsx.jxzhpt.gcgl.bean.Gcglabgc;
+import com.hdsx.jxzhpt.gcgl.bean.Gcglwqgz;
+import com.hdsx.jxzhpt.gcgl.server.GcglabgcServer;
+import com.hdsx.jxzhpt.gcgl.server.GcglwqgzServer;
+import com.hdsx.jxzhpt.jhgl.bean.Plan_gcgj;
+import com.hdsx.jxzhpt.jhgl.bean.Plan_lx_gcgj;
+import com.hdsx.jxzhpt.jhgl.controller.ImportVerify;
+import com.hdsx.jxzhpt.utile.EasyUIPage;
+import com.hdsx.jxzhpt.utile.ExcelReader;
+import com.hdsx.jxzhpt.utile.ExcelReader1;
+import com.hdsx.jxzhpt.utile.JsonUtils;
+import com.hdsx.jxzhpt.utile.ResponseUtils;
+import com.hdsx.jxzhpt.wjxt.bean.Lkmxb;
+import com.hdsx.jxzhpt.wjxt.bean.Trqk;
+import com.hdsx.jxzhpt.wjxt.server.TrqkServer;
+import com.hdsx.jxzhpt.xtgl.bean.Master;
+import com.hdsx.jxzhpt.xtgl.bean.Plan_flwbzbz;
+import com.hdsx.webutil.struts.BaseActionSupport;
+
+@Scope("prototype")
+@Controller
+public class LkpdController extends BaseActionSupport{
+	private int page = 1;
+	private int rows = 10;
+	@Resource(name = "trqkServerImpl")
+	private TrqkServer trqkServer;
+	private String gydw;
+	private String tiaojian;
+	private Trqk trqk=new Trqk();
+	private Lkmxb lkmxb=new Lkmxb();
+	private File fileupload;
+	private String fileuploadFileName;
+
+	public Lkmxb getLkmxb() {
+		return lkmxb;
+	}
+
+	public void setLkmxb(Lkmxb lkmxb) {
+		this.lkmxb = lkmxb;
+	}
+
+	public String getGydw() {
+		return gydw;
+	}
+
+	public void setGydw(String gydw) {
+		this.gydw = gydw;
+	}
+
+	public String getTiaojian() {
+		return tiaojian;
+	}
+
+	public void setTiaojian(String tiaojian) {
+		this.tiaojian = tiaojian;
+	}
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public Trqk getTrqk() {
+		return trqk;
+	}
+
+	public void setTrqk(Trqk trqk) {
+		this.trqk = trqk;
+	}
+	
+	
+	public File getFileupload() {
+		return fileupload;
+	}
+
+	public void setFileupload(File fileupload) {
+		this.fileupload = fileupload;
+	}
+
+	public String getFileuploadFileName() {
+		return fileuploadFileName;
+	}
+
+	public void setFileuploadFileName(String fileuploadFileName) {
+		this.fileuploadFileName = fileuploadFileName;
+	}
+
+	public void insertLkpdData(){
+		System.out.println("进入方法");
+		String fileType=fileuploadFileName.substring(fileuploadFileName.length()-3, fileuploadFileName.length());
+		System.out.println("文件类型："+fileType);
+		HttpServletResponse response = ServletActionContext.getResponse();
+		try{
+			if(!"xls".equals(fileType)){
+				response.getWriter().print(fileuploadFileName+"不是excel文件");
+				return ;
+			}
+			response.setCharacterEncoding("utf-8"); 
+			FileInputStream fs = new FileInputStream(this.fileupload);
+			List<Map>[] dataMapArray;
+			try{
+				dataMapArray = ExcelReader1.readExcelContent(2,17,fs,Plan_gcgj.class);
+
+			}catch(Exception e){
+				response.getWriter().print(fileuploadFileName+"数据有误");
+				return;
+			}
+			List<Map> data = ExcelReader1.removeBlankRow(dataMapArray[0]);
+			String strVerify="";
+			boolean boolJh=false,boolLx=false;
+
+			System.out.println(data);
+			data.remove(0);
+			data.remove(0);
+			data.remove(0);
+			data.remove(data.size()-1);
+			System.out.println(data);
+			if(boolJh && boolLx)
+				response.getWriter().print(fileuploadFileName+"导入成功");
+			else 
+				response.getWriter().print(fileuploadFileName+"导入失败\r"+strVerify);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
--- jxzhpt/src/main/webapp/page/lkpd/js/lkpd.js
+++ jxzhpt/src/main/webapp/page/lkpd/js/lkpd.js
@@ -3,7 +3,7 @@ function insertData(flag){
 	var weatherDlg = new J.dialog( {
 		id : 'id2',
 		title : '安全管理月报表添加',
-		page : 'upload.jsp?url='+"/jxzhpt/gcgl/insertData.do"+'&flag='+flag,
+		page : 'upload.jsp?url='+"/jxzhpt/wjxt/insertLkpdData.do"+'&flag='+flag,
 		width : 570,
 		height : 440,
 		top : 0,
