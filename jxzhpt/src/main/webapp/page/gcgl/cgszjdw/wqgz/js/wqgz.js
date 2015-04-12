c0751de067f542f0b46c0a154493e35ea75a6250
Administrator <Administrator@9K9U9VZ2UBKQ2K7>
2015-04-12 17:04:51 +0800

    qwh

--- jxzhpt/src/main/webapp/page/gcgl/cgszjdw/wqgz/js/wqgz.js
+++ jxzhpt/src/main/webapp/page/gcgl/cgszjdw/wqgz/js/wqgz.js
@@ -271,7 +271,7 @@ function jiazaifujian(data1){
 				if(msg.jgyswj!=''){
 					$("#xz_jgyswj").text(msg.jgyswj);
 					$("#xz_jgyswj").attr("style",'color: #2C7ED1;cursor:pointer;');
-					$("#xz_jgtcwj").attr("href",'/jxzhpt/gcgl/downWqgzFile.do?type=jgyswj'+"&jhid="+parent.obj1.jhid);
+					$("#xz_jgyswj").attr("href",'/jxzhpt/gcgl/downWqgzFile.do?type=jgyswj'+"&jhid="+parent.obj1.jhid);
 				}
 			}
 	});	
