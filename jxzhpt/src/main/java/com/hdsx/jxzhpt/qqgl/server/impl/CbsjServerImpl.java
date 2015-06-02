5f900095bf88fc678d61a1cd095840437bdcb845
admin <admin@LrLuckly>
2015-06-02 20:40:38 +0800

    renhao

--- jxzhpt/src/main/java/com/hdsx/jxzhpt/qqgl/server/impl/CbsjServerImpl.java
+++ jxzhpt/src/main/java/com/hdsx/jxzhpt/qqgl/server/impl/CbsjServerImpl.java
@@ -1,5 +1,6 @@
 package com.hdsx.jxzhpt.qqgl.server.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,7 +8,8 @@ import java.util.Map;
 import org.springframework.stereotype.Service;
 
 import com.hdsx.dao.query.base.BaseOperate;
-import com.hdsx.jxzhpt.qqgl.bean.CbsjLmsj;
+import com.hdsx.jxzhpt.jhgl.bean.Plan_upload;
+import com.hdsx.jxzhpt.qqgl.bean.Cbsj;
 import com.hdsx.jxzhpt.qqgl.server.CbsjServer;
 @Service
 public class CbsjServerImpl extends BaseOperate implements CbsjServer {
@@ -16,16 +18,46 @@ public class CbsjServerImpl extends BaseOperate implements CbsjServer {
 	}
 	private Map<String, Object> params=new HashMap<String, Object>();
 	@Override
-	public List<CbsjLmsj> queryCbsjLmsj(CbsjLmsj lmsj, int page, int rows) {
+	public List<Cbsj> queryCbsjLmgz(Cbsj lmsj, int page, int rows) {
 		params.put("lmsj", lmsj);
 		params.put("page", page);
 		params.put("rows", rows);
-		return queryList("queryCbsjLmsj",params);
+		return queryList("queryCbsjLmgz",params);
 	}
 	@Override
-	public int queryCbsjLmsjCount(CbsjLmsj lmsj) {
+	public int queryCbsjLmgzCount(Cbsj lmsj) {
 		params.put("lmsj", lmsj);
-		return queryOne("queryCbsjLmsjCount", params);
+		return queryOne("queryCbsjLmgzCount", params);
+	}
+	@Override
+	public Cbsj queryCbsjLmgzByXmbm(String xmbm) {
+		params.put("xmbm", xmbm);
+		return queryOne("queryCbsjLmgzByXmbm",params);
+	}
+	@Override
+	public boolean updateCbsjLmgz(Cbsj cbsj) {
+		return update("updateCbsjLmgz",cbsj)>0;
+	}
+	@Override
+	public boolean insertFile(Plan_upload uploads) {
+		return insert("insertFile", uploads)>0;
+	}
+	@Override
+	public List<Plan_upload> queryFileByXmbm(String xmbm) {
+		params.put("xmbm", xmbm);
+		return queryList("queryFileByXmbm",params);
+	}
+	@Override
+	public boolean deleteLmgzByXmbm(String xmbm) {
+		System.out.println("项目编码："+xmbm);
+		String[] split = xmbm.split(",");
+		List<Cbsj> list=new ArrayList<Cbsj>();
+		for (String string : split) {
+			Cbsj obj=new Cbsj();
+			obj.setXmbm(string);
+			list.add(obj);
+		}
+		return deleteBatch("deleteLmgzByXmbm",list)==list.size();
 	}
 
 }
--- jxzhpt/src/main/webapp/page/qqgl/js/util.js
+++ jxzhpt/src/main/webapp/page/qqgl/js/util.js
@@ -1,3 +1,19 @@
+//-------------------------------------------用户等级判断部分
+/**
+ * 判断用户行政登记(省、市、县)
+ * @param unit2 cookie unit2中的值
+ * @returns {String} 返回行政登记(省、市、县)
+ */
+function userPanduan(unit2){
+	if(unit2.length==7){
+		return "省";
+	}else if(unit2.length==9){
+		return "市";
+	}else if(unit2.length==11){
+		return "县";
+	}
+}
+//-------------------------------------------公用方法
 /**
  * 加载特殊地区下拉列表
  * @param id 元素ID
@@ -7,7 +23,78 @@ function loadTsdq(id){
 	    url:'../../../jhgl/queryTsdq.do',
 	    valueField:'id',
 	    textField:'text'
-	}); 
+	});
+}
+/**
+ * 查询设计批复文件
+ * @param xmbm
+ */
+function fileShow(xmbm){
+	$.ajax({
+		type:'post',
+		url:'../../../qqgl/queryFileByXmbm.do',
+		data:'cbsj.xmbm='+xmbm,
+		dataType:'json',
+		success:function(data){
+			$("#sjpfTable").empty();
+			for ( var i = 0; i < data.length; i++) {
+				var tr = "<tr><td style='background-color: #ffffff; height: 25px;' align='left'>" + data[i].filename +"</td><td style='background-color: #ffffff; height: 25px;' align='left'><a href='javascript:void(0)'style='text-decoration:none;color:#3399CC; ' onclick=downFile('"+data[i].id+"')>下载</a>  |  <a href='javascript:void(0)'style='text-decoration:none;color:#3399CC; ' onclick=deleteFile('"+data[i].id+"')>删除</a></td></tr>";
+				$("#sjpfTable").append(tr);
+			}
+		}
+	});
+}
+/**
+ * 下载文件
+ * @param id 文件ID
+ */
+function downFile(id){
+	parent.window.location.href="/jxzhpt/jhgl/downAbgcFile.do?uploads.id="+id;
+}
+/**
+ * 删除文件
+ * @param id 文件ID
+ */
+function deleteFile(id){
+	if(confirm('确定删除所选数据？')){
+		$.ajax({
+			 type : "POST",
+			 url : "/jxzhpt/jhgl/deleteFile.do",
+			 dataType : 'json',
+			 data : 'uploads.id=' +id,
+			 success : function(msg){
+				 if(msg){
+					 alert('删除成功！');
+					 fileShow(parent.YMLib.Var.xmbm);
+				 }else{
+					 YMLib.Tools.Show('删除失败,请选择要删除数据！',3000);
+				 }
+			 },
+			 error : function(){
+				 YMLib.Tools.Show('服务器请求无响应！error code = 404',3000);
+			 }
+		});
+	}
+}
+//最新选择的行的项目编码
+/**
+ * 弹出窗口
+ * @param id 窗口ID
+ * @param title 窗口标题
+ * @param url 窗口页面的路径
+ * @param width 窗口的宽度
+ * @param height 窗口的高度
+ */
+function openWindow(id,title,url,width,height){
+	YMLib.Var.xmbm=xmbm;
+	YMLib.UI.createWindow(id,title,url,id,width,height);
+}
+/**
+ * 关闭弹出窗口
+ * @param id
+ */
+function closeWindow(id){
+	parent.$('#'+id).window("destroy");
 }
 /**
  * 获取管养单位选中值
@@ -39,21 +126,35 @@ function getxzqhdm(id){
 	}
 	return result;
 }
+//--------------------------------------------------------easyui部分
+var gridObj;//datagrid 对象
+var Rh={
+		onLoadSuccess:function(data){
+		},
+		onClickRow:function(rowIndex, rowData){
+		},
+		onSelect:function(rowIndex, rowData){
+			xmbm=rowData.xmbm;
+			selArray.push(rowData.xmbm);
+		},
+		onUnselect:function(rowIndex, rowData){
+			xmbm=rowData.xmbm;
+			selArray.pop(rowData.xmbm);
+		}
+	};
 //此对象为绑定列表对象
 var grid={
 		url:null,queryParams:null,height:null,width:null,
-		columns:null,onSelect:null,onClickRow:null,
-		striped:true,pagination:true,
-		rownumbers:true,pageNumber:pageNumber,
-		pageSize:pageSize,onLoadSuccess:null,
-		view:null,detailFormatter:null,onExpandRow:null};
-var gridObj;
+		columns:null,striped:true,pagination:true,
+		rownumbers:true,pageNumber:1,pageSize:10
+		/*,view:null,detailFormatter:null,onExpandRow:null*/};
+var xmbm;//最新选择的项目编码
+var selArray=new Array();//选中的项目编码集合
 /**
  * 绑定easy-ui的datagrid表格
  * @param grid datagrid配置对象
  */
 function gridBind(grid){
-	alert(grid.pagination);
 	gridObj = $('#'+grid.id).datagrid({
 	    url:grid.url,
 	    queryParams:grid.queryParams,
@@ -65,9 +166,10 @@ function gridBind(grid){
 	    height:grid.height,
 	    width:grid.width,
 	    columns:grid.columns,
-	    onSelect:grid.onSelect,
-	    onClickRow:grid.onClickRow,
-	    onLoadSuccess:grid.onLoadSuccess
+	    onSelect:Rh.onSelect,
+	    onUnselect:Rh.onUnselect,
+	    onClickRow:Rh.onClickRow,
+	    onLoadSuccess:Rh.onLoadSuccess
 	    /*view:grid.view,
 	    detailFormatter:grid.detailFormatter,
 	    onExpandRow:grid.onExpandRow,*/
