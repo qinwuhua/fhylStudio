17ebcd2a9396d5bc70f4330e59154c3d38797647
w_borlin <715416358@qq.com>
2017-03-13 11:18:06 +0800

    增加导出excel表头

--- jxzhpt/src/main/java/ConnectionUtil.java
+++ jxzhpt/src/main/java/ConnectionUtil.java
@@ -39,13 +39,13 @@ public class ConnectionUtil {
   Connection conn = cu.getConnection();  
   ResultSet rs = null; 
   //就是我给你发的sql文件里面的sql语句。
-  String sql=" select t2.value, t2.name, t1.co, t2.hight, t2.rowxh  from (select substr1 value, count(*) co  from aaa  where value in   ('41v_59', '41v_60', '41v_61', '41v_62', '41v_63', '41v_64',   '42v_69', '431v_70', '431v_71', '431v_72', '431v_73', '431v_74', '432v_75',  '432v_76', '433v_77', '44v_78', '45v_79', '46v_80', '47v_81')"+
-		  " group by substr1  union all select substr2 value, count(*) co  from aaa  where value in ('41v_59', '41v_60', '41v_61', '41v_62', '41v_63', '41v_64','42v_69', '431v_70',  '431v_71', '431v_72', '431v_73', '431v_74', '432v_75', '432v_76', '433v_77', '44v_78', '45v_79', '46v_80', '47v_81')"+
-		  " and substr2 is not null  group by substr2 union all select substr3 value, count(*) co from aaa where value in ('41v_59', '41v_60', '41v_61', '41v_62', '41v_63', '41v_64','42v_69', '431v_70', '431v_71', '431v_72', '431v_73', '431v_74', '432v_75','432v_76', '433v_77', '44v_78', '45v_79', '46v_80', '47v_81')"+
-		  " and substr3 is not null group by substr3  union all select value, count(*) co from aaa where value in ('41v_59', '41v_60', '41v_61', '41v_62', '41v_63', '41v_64',  '42v_69', '431v_70', '431v_71', '431v_72', '431v_73', '431v_74', '432v_75','432v_76', '433v_77', '44v_78', '45v_79', '46v_80', '47v_81')"+
-		  " group by value) t1, aaa t2 where t1.value = t2.value order by t2.xh";
+  String sql=" select t2.value, t2.name, t1.co, t2.hight, t2.rowxh  from (select substr1 value, count(*) co  from ZDYBBBT  where value in   ('41v_59', '41v_60', '41v_61', '41v_62', '41v_63', '41v_64',   '42v_69', '431v_70', '431v_71', '431v_72', '431v_73', '431v_74', '432v_75',  '432v_76', '433v_77', '44v_78', '45v_79', '46v_80', '47v_81')"+
+		  " group by substr1  union all select substr2 value, count(*) co  from ZDYBBBT  where value in ('41v_59', '41v_60', '41v_61', '41v_62', '41v_63', '41v_64','42v_69', '431v_70',  '431v_71', '431v_72', '431v_73', '431v_74', '432v_75', '432v_76', '433v_77', '44v_78', '45v_79', '46v_80', '47v_81')"+
+		  " and substr2 is not null  group by substr2 union all select substr3 value, count(*) co from ZDYBBBT where value in ('41v_59', '41v_60', '41v_61', '41v_62', '41v_63', '41v_64','42v_69', '431v_70', '431v_71', '431v_72', '431v_73', '431v_74', '432v_75','432v_76', '433v_77', '44v_78', '45v_79', '46v_80', '47v_81')"+
+		  " and substr3 is not null group by substr3  union all select value, count(*) co from ZDYBBBT where value in ('41v_59', '41v_60', '41v_61', '41v_62', '41v_63', '41v_64',  '42v_69', '431v_70', '431v_71', '431v_72', '431v_73', '431v_74', '432v_75','432v_76', '433v_77', '44v_78', '45v_79', '46v_80', '47v_81')"+
+		  " group by value) t1, ZDYBBBT t2 where t1.value = t2.value order by t2.xh";
 		   
-		  //"select t2.value,t2.name,t1.co,t2.hight,t2.rowxh from(select substr1 value,count(*) co from aaa where INSTR(value, 'v_', -1, 1)>0 group by substr1 union all select substr2 value,count(*) co from aaa where INSTR(value, 'v_', -1, 1)>0 and substr2 is not null group by substr2 union all select substr3 value,count(*) co from aaa where INSTR(value, 'v_', -1, 1)>0 and substr3 is not null group by substr3 union all select value,count(*) co from aaa where INSTR(value, 'v_', -1, 1)>0 group by value )t1,aaa t2 where t1.value=t2.value order by t2.xh";
+		  //"select t2.value,t2.name,t1.co,t2.hight,t2.rowxh from(select substr1 value,count(*) co from ZDYBBBT where INSTR(value, 'v_', -1, 1)>0 group by substr1 union all select substr2 value,count(*) co from ZDYBBBT where INSTR(value, 'v_', -1, 1)>0 and substr2 is not null group by substr2 union all select substr3 value,count(*) co from ZDYBBBT where INSTR(value, 'v_', -1, 1)>0 and substr3 is not null group by substr3 union all select value,count(*) co from ZDYBBBT where INSTR(value, 'v_', -1, 1)>0 group by value )t1,ZDYBBBT t2 where t1.value=t2.value order by t2.xh";
 
   try {  
       ps = conn.prepareStatement(sql);  
@@ -56,17 +56,17 @@ public class ConnectionUtil {
     	  
               while (rs.next()) {  
             	 //System.out.println(rs.getString(1)+"--"+rs.getString(2)+"--"+rs.getString(3)+"--"+rs.getString(4)+"--"+rs.getString(5)+"--"); ;
-            	  AA aa=new AA(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5));
+            	  AA aa=new AA(rs.getString(1),rs.getString(2),rs.getInt(3),rs.getInt(4),rs.getInt(5));
             	  list.add(aa);
               }
               
       }
       
-     //以上代码就是为了获取SQL语句的查询结果，并且封装到了一个实体里面。以下的这一段代码是在拼接表头。
+     /*//以上代码就是为了获取SQL语句的查询结果，并且封装到了一个实体里面。以下的这一段代码是在拼接表头。
       String html="<tr>";
-      String rowxh="0";
+      int rowxh=0;
       for(int i=0;i<list.size();i++){
-    	  if(!rowxh.equals(list.get(i).getRowxh())){
+    	  if(rowxh!=list.get(i).getRowxh()){
     		  html=html+"</tr><tr>";
     		  rowxh=list.get(i).getRowxh();
     	  }
@@ -74,7 +74,27 @@ public class ConnectionUtil {
       }
       html=html+"</tr>";
       //把拼接的内容复制到了html里面，查看效果
-      System.out.println(html);
+      System.out.println(html);*/
+      
+      int col1=0,col2=0;
+      int rowxh=1;
+      for(int i=0;i<list.size();i++){
+    	  if(rowxh!=list.get(i).getRowxh()){
+    		  col1=0;
+    		  rowxh=list.get(i).getRowxh();
+    	  }
+    	  list.get(i).setRow1(list.get(i).getRowxh()-1);
+    	  list.get(i).setRow2(list.get(i).getRowxh()-1+list.get(i).getHi()-1);
+    	  list.get(i).setCol1(col1);
+    	  col2=col1+list.get(i).getCol()-1;
+    	  list.get(i).setCol2(col2);
+    	  col1=col2+1;
+      }
+      
+      for(int i=0;i<list.size();i++){
+    	  System.out.println(list.get(i).getName()+":"+list.get(i).getRow1()+":"+list.get(i).getRow2()+":"+list.get(i).getCol1()+":"+list.get(i).getCol2());
+      }
+      
   } catch (SQLException e) {  
       // TODO Auto-generated catch block  
       e.printStackTrace();  
@@ -87,9 +107,11 @@ public class ConnectionUtil {
 class AA{
 	String value;
 	String name;
-	String col;
-    String hi;
-    String rowxh;
+	int col;
+    int hi;
+    int rowxh;
+    
+    int row1,row2,col1,col2;
 	public String getValue() {
 		return value;
 	}
@@ -102,25 +124,53 @@ class AA{
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getCol() {
+	
+	
+	
+	
+	public int getCol() {
 		return col;
 	}
-	public void setCol(String col) {
+	public void setCol(int col) {
 		this.col = col;
 	}
-	public String getHi() {
+	public int getHi() {
 		return hi;
 	}
-	public void setHi(String hi) {
+	public void setHi(int hi) {
 		this.hi = hi;
 	}
-	public String getRowxh() {
+	public int getRowxh() {
 		return rowxh;
 	}
-	public void setRowxh(String rowxh) {
+	public void setRowxh(int rowxh) {
 		this.rowxh = rowxh;
 	}
-	public AA(String value, String name, String col, String hi, String rowxh) {
+	public int getRow1() {
+		return row1;
+	}
+	public void setRow1(int row1) {
+		this.row1 = row1;
+	}
+	public int getRow2() {
+		return row2;
+	}
+	public void setRow2(int row2) {
+		this.row2 = row2;
+	}
+	public int getCol1() {
+		return col1;
+	}
+	public void setCol1(int col1) {
+		this.col1 = col1;
+	}
+	public int getCol2() {
+		return col2;
+	}
+	public void setCol2(int col2) {
+		this.col2 = col2;
+	}
+	public AA(String value, String name, int col, int hi, int rowxh) {
 		super();
 		this.value = value;
 		this.name = name;
