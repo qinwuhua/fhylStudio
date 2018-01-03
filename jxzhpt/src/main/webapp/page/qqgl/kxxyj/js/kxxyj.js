029e26108b1e59966c88f975420a50e43d331f28
Administrator <Administrator@pgos>
2018-01-03 14:30:19 +0800

    修改路面改造工可审核无效

--- jxzhpt/src/main/webapp/page/qqgl/kxxyj/js/kxxyj.js
+++ jxzhpt/src/main/webapp/page/qqgl/kxxyj/js/kxxyj.js
@@ -1772,7 +1772,7 @@ function showAlllmsh(){
 	        		
 	        		if(row.scshzt == '1'){
 		        		if(row.sbzt1=='0'){
-			        		return '<a style="text-decoration:none;color:#3399CC;" href="#" onclick="shenh('+index+')">未审核</a>';
+			        		return '<a style="text-decoration:none;color:#3399CC;" href="#" onclick="shenh1('+index+')">未审核</a>';
 		        		}
 	        		}else{
 		        		if(row.sbzt1=='0'){ return '未审核'; }  
